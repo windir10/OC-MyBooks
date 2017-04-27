6c580ed760a6732e34d0394e6179bd054b203c3f
Steven DUMONT <windir10@gmail.com>
2017-04-27 11:24:57 +0200

    create view templates + styling CSS + dev routing php

--- app/routes.php
+++ app/routes.php
@@ -1,2 +1,13 @@
 <?php
 
+// Home page
+$app->get('/', function () use ($app) {
+    $books = $app['dao.book']->findAll();
+    return $app['twig']->render('index.html.twig', array('books' => $books));
+})->bind('home');
+
+// Book details page
+$app->get('/book/{id}', function ($id) use ($app) {
+    $book = $app['dao.book']->find($id);
+    return $app['twig']->render('book.html.twig', array('book' => $book));
+})->bind('book');
